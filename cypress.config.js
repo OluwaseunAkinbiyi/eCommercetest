db5d3695adbc03bdcd67cb6db2ae98299296eaaf
oluwaseun11122 <justhatboyondlow@gmail.com>
2024-10-26 12:57:23 -0700

    Initial commit of Cypress project

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,23 @@ module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
+    },
+  },
+});
     },
   },
 };
